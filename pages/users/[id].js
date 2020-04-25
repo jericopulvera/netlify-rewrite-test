4ecf1413b07d0e6bdd31bfaf591efb010cdf2168
ECO <jerico.pulvera@outlook.com>
2020-04-25 18:10:00 +0800

    chore: minor changes

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,7 +1,15 @@
-
+import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Page({ url }) {
+  const router = useRouter()
+
+  React.useEffect(() => {
+    console.log('Component Did Mount!')
+    console.log(router)
+  }, [])
+
   return (
       <div>
          <Link href="/">
